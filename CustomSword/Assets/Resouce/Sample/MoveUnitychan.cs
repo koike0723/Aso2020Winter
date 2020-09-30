7519d50ee1841abd295df12b916a28503bc7cb2d
koike_asopc <koike.y44@gmail.com>
2020-09-30 15:21:31 +0900

    inputsystemの導入と研究

---
+++ CustomSword/Assets/Resouce/Sample/MoveUnitychan.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoveUnitychan : MonoBehaviour
+{
+
+    private UnitychanControls _input;
+    Animator _animator;
+
+    private void Awake()
+    {
+        // アセット名と同名のクラスを生成する
+        _input = new UnitychanControls();
+    }
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        // 使用する前に有効化する必要がある
+        _input.Enable();
+    }
+
+    void Update()
+    {
+        // PhaseがPerformedのときにログ出力
+        if (_input.Player.MoveLeft.ReadValue<float>() >= 0)
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            transform.position += transform.forward * 0.1f;
+            _animator.SetBool("is_running", true);
+            Debug.Log("left");
+        }
+        else
+        {
+            _animator.SetBool("is_running", false);
+        }
+
+    }
+
+    private void OnDisable()
+    {
+        // 使用が終わったら無効化する
+        _input.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        _input.Dispose();
+    }
+
+    
+   
+}
---
+++ CustomSword/Assets/Resouce/Sample/UnitychanControls.cs
@@ -0,0 +1,132 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Resouce/Sample/UnitychanControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @UnitychanControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @UnitychanControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""UnitychanControls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""9b3b6e78-7157-4847-85ad-b66d9b6ca801"",
+            ""actions"": [
+                {
+                    ""name"": ""MoveLeft"",
+                    ""type"": ""Value"",
+                    ""id"": ""6332590f-dac4-44af-aa5b-558e1545282e"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""d278354b-be61-46f2-82e7-5f2b993a70a2"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveLeft"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_MoveLeft = m_Player.FindAction("MoveLeft", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_MoveLeft;
+    public struct PlayerActions
+    {
+        private @UnitychanControls m_Wrapper;
+        public PlayerActions(@UnitychanControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @MoveLeft => m_Wrapper.m_Player_MoveLeft;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @MoveLeft.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveLeft;
+                @MoveLeft.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveLeft;
+                @MoveLeft.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveLeft;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @MoveLeft.started += instance.OnMoveLeft;
+                @MoveLeft.performed += instance.OnMoveLeft;
+                @MoveLeft.canceled += instance.OnMoveLeft;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMoveLeft(InputAction.CallbackContext context);
+    }
+}
